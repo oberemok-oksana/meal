ff5937be28a6b4a81600760ac218086e85f4a4ad
Oksana Oberemok <Pylypenkok@gmail.com>
2022-12-01 21:37:43 +0700

    replace code from App to Home

---
+++ src/pages/Home.js
@@ -0,0 +1,61 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Card from "../components/Card";
+import { findMealByName } from "../api/index";
+
+const Home = () => {
+  const [mealName, setMealName] = useState("");
+  const [search, setSearch] = useState("");
+  const { data: mealsData, isLoading } = useQuery(["mealData", search], () =>
+    findMealByName(mealName)
+  );
+
+  const changeMealName = (e) => {
+    setMealName(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setSearch(mealName);
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="main-title">What meal do you prefer today?</h1>
+        <form className="search-form" onSubmit={submitForm}>
+          <input
+            type="text"
+            placeholder="Type name of a meal"
+            name="search-input"
+            className="search-form__input"
+            value={mealName}
+            onChange={changeMealName}
+          />
+          <button type="submit" className="search-form__btn">
+            Search
+          </button>
+        </form>
+        <ul className="meals">
+          {mealsData.map((item) => (
+            <Card
+              key={item.idMeal}
+              id={item.idMeal}
+              title={item.strMeal}
+              area={item.strArea}
+              category={item.strCategory}
+              src={item.strMealThumb}
+              item={item}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
