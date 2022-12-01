78b9caf943fa78b301377170668e2634cb09f7b6
Oksana Oberemok <Pylypenkok@gmail.com>
2022-12-01 21:37:55 +0700

    add Route Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
