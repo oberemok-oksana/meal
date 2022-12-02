c280caca443a5d8846b64c4407f51e38d6b24b09
Oksana Oberemok <Pylypenkok@gmail.com>
2022-12-02 20:50:19 +0700

    make filterIngredients, filterMeasures, cutYoutubeLink

---
+++ src/helpers.js
@@ -0,0 +1,31 @@
+export const filterIngredients = (item) => {
+  const ingredients = [];
+  for (let ingredient in item) {
+    if (ingredient.startsWith("strIng") && item[ingredient] !== "") {
+      ingredients.push(item[ingredient]);
+    }
+  }
+  return ingredients;
+};
+
+export const filterMeasures = (item) => {
+  const measures = [];
+  for (let measure in item) {
+    if (
+      measure.startsWith("strMeasure") &&
+      item[measure] !== " " &&
+      measure.startsWith("strMeasure") &&
+      item[measure] !== ""
+    ) {
+      measures.push(item[measure]);
+    }
+  }
+  return measures;
+};
+
+export const cutYoutubeLink = (text) => {
+  const index = text.split("").findIndex((letter) => letter === "=");
+  return text.slice(index + 1);
+};
+
+//"https://www.youtube.com/watch?v=YsJXZwE5pdY"
