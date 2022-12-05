cc52cf836c516f020ef5005f0e238d8b28ab5fea
Oksana Oberemok <Pylypenkok@gmail.com>
2022-12-05 21:15:22 +0700

    add BASE for api address

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,17 @@
+const BASE = "http://www.themealdb.com/api/json/v1/1";
+
 export const findMealByName = (name) => {
-  return fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+  return fetch(`${BASE}/search.php?s=${name}`)
     .then((response) => response.json())
     .then((data) => data.meals);
 };
 
 export const getInfoById = (id) => {
-  return fetch(`http://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+  return fetch(`${BASE}/lookup.php?i=${id}`)
     .then((response) => response.json())
     .then((data) => data.meals[0]);
 };
 
 export const getRandomMeal = () => {
-  return fetch("www.themealdb.com/api/json/v1/1/random.php").then((response) =>
-    response.json()
-  );
+  return fetch(`${BASE}/random.php`).then((response) => response.json());
 };
